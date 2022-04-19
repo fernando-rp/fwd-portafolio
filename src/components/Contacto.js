63b3d32a5137a25a1fa58a58d702c84089dcd43a
Fernando <fernandor.p@hotmail.com>
2022-04-19 09:55:00 -0400

    contact modified

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -43,24 +43,24 @@ return(
         <h2 className="title-section text-center mb-4" id="contacto">Escríbeme</h2>
         <p className="text-contacto mb-4 p-4">Si te ha interesado mi portafolio y deseas saber más de mi perfil, por favor comunícate conmigo completando el formulario. Por otra parte, si revisaste mi portafolio y visualizaste puntos de mejora, también te agradecería enormemente que me lo comunicaras.</p>
         
-        <form className="form p-4 needs-validation" id="contacto" onSubmit={formSubmitted}>
+        <form className="form p-4 needs-validation" name="contact" id="contacto" onSubmit={formSubmitted} netfily>
             <div className="row">
                 <div className="mb-3 col-6">
                     <label className="form-label">Nombre</label>
-                    <input type="text" className="form-control" id="validationName" onChange={takeName} value={enteredName} required />
+                    <input type="text" name="name" className="form-control" id="validationName" onChange={takeName} value={enteredName} required />
                 </div>
                 <div className="mb-3 col-6">
                     <label className="form-label">Apellido</label>
-                    <input type="text" className="form-control" onChange={takeLastName} value={enteredLastName} required />
+                    <input type="text" name="last-name" className="form-control" onChange={takeLastName} value={enteredLastName} required />
                 </div>
             </div>
             <div className="mb-3">
                 <label className="form-label">Email</label>
-                <input type="email" className="form-control" placeholder="name@example.com" onChange={takeEmail} value={enteredEmail} required />
+                <input type="email" name="email" className="form-control" placeholder="name@example.com" onChange={takeEmail} value={enteredEmail} required />
             </div>
             <div className=" d-flex flex-column mb-1">
                 <label className="form-label">Mensaje</label>
-                <textarea className="form-control" rows="5" onChange={takeMessage} value={enteredMessage} required></textarea>
+                <textarea className="form-control" name="message" rows="5" onChange={takeMessage} value={enteredMessage} required></textarea>
             </div>
             <div className="d-flex flex-column mb-4 my-4">
                 <button type="submit" className="btn fw-bold ">Enviar</button>
