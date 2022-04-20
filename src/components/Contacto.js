4f6421f5e9cdc07a2f357d0cfe829bbd919489fe
Fernando <fernandor.p@hotmail.com>
2022-04-19 22:09:57 -0400

    favicon modified

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -22,22 +22,22 @@ function Contacto(){
 
 
 
-    const formSubmitted=(event)=>{
+    // const formSubmitted=(event)=>{
 
-        event.preventDefault();
-        alert("probando")
-        setEnteredName('')
-        setEnteredLastName('')
-        setEmail('')
-        setMessage('')
-    }
+    //     event.preventDefault();
+    //     alert("probando");
+    //     setEnteredName('')
+    //     setEnteredLastName('')
+    //     setEmail('')
+    //     setMessage('')
+    // }
 
 return(
     <div className="container my-4">
         <h2 className="title-section text-center mb-4" id="contacto">Escríbeme</h2>
         <p className="text-contacto mb-4 p-4">Si te ha interesado mi portafolio y deseas saber más de mi perfil, por favor comunícate conmigo completando el formulario. Por otra parte, si revisaste mi portafolio y visualizaste puntos de mejora, también te agradecería enormemente que me lo comunicaras.</p>
         
-        <form className="form p-4 needs-validation" name="contact-form" id="contacto"  method="POST" data-netlify="true" action={formSubmitted}>
+        <form className="form p-4 needs-validation" name="contact-form" id="contacto"  method="POST" data-netlify="true">
             <input type="hidden" name="form-name" value="contact-form" />
             <div className="row">
                 <div className="mb-3 col-6">
